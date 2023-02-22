93b16dca446eefc4e1fc3ce96afafae80d303337
DevrMichael <88589247+DevrMichael@users.noreply.github.com>
2023-02-23 00:02:10 +0100

    Initialize Barber Site Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <nav>
+          <a href="">Frisør</a>
+          <a href="">Bestill Time</a>
+          <a href="">Kontakt Oss</a>
+          <a href="">Prisliste</a>
+          <a href="">Om Oss</a>
+        </nav>
       </header>
-    </div>
+      <div className="hero-container">
+        <div className="hero hero-container-background">
+          <h1>Våre Frisører Hjelper Deg</h1>
+          <h2>Finn Din Frisør Hos Oss I Dag. Bestill Time Nå!</h2>
+          <button>Bestill Time</button>
+        </div>
+      </div>
+      <div className="features">
+        <div>
+          <h2>Bestill Time</h2>
+        </div>
+        <div>
+          <h2>Instagram</h2>
+        </div>
+        <div>
+          <h2>Kontakt Oss</h2>
+        </div>
+      </div>
+    </>
   );
 }
 
