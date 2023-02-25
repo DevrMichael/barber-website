a4096fa7ecd130c54ed322aea2ed11c0b6c12c5c
DevrMichael <88589247+DevrMichael@users.noreply.github.com>
2023-02-25 15:21:59 +0100

    Add final components + finalize project

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
           </div>
         </div>
       </div>
-      <div className="features">
+      <div className="features-container">
         <div>
           <h2>Bestill Time</h2>
         </div>
@@ -35,10 +35,10 @@ function App() {
           <h2>Kontakt Oss</h2>
         </div>
       </div>
-      <div className="customers-header">
+      <div className="customers-header-container">
         <h3>Eksempler på våre fornøyde kunder:</h3>
       </div>
-      <div className="customers-image-slider">
+      <div className="customers-image-slider-container">
         <div className="slider-images">
           <img src={imageOne} alt="First img" />
           <img src={imageTwo} alt="Second img" />
@@ -51,7 +51,7 @@ function App() {
           <img src={imageThree} alt="Third img" />
         </div>
       </div>
-      <div className="presentation">
+      <div className="presentation-container">
         <h2>
           Hvorfor velge <br />
           <span>Hendrix Hair</span>
@@ -76,33 +76,37 @@ function App() {
         </p>
         <p>Kom innom for en uforpliktende konsultasjon.</p>
       </div>
-      <div className="map-container">
-        <div className="map">
-          <div>
-            <p>Klassisk hårklipp</p>
-            <p>fra 550,-</p>
+      <div className="price-container">
+        <div>
+          <div className="prices">
+            <div className="prices-text">
+              <p>Klassisk hårklipp</p>
+              <p>fra 550,-</p>
+            </div>
+            <div className="prices-text">
+              <p>Barbering med kniv</p>
+              <p>fra 450,-</p>
+            </div>
+            <div className="prices-text">
+              <p>Skjeggtrimming</p>
+              <p>fra 500,-</p>
+            </div>
+            <div className="prices-text">
+              <p>Barneklipp</p>
+              <p>fra 400,-</p>
+            </div>
+            <div className="prices-text">
+              <p>Senior hårklipp</p>
+              <p>fra 400,-</p>
+            </div>
+            <div className="prices-buttons">
+              <a href="">Bestill Time</a>
+              <a href="">Les Mer</a>
+            </div>
           </div>
-          <div>
-            <p>Barbering med kniv</p>
-            <p>fra 450,-</p>
-          </div>
-          <div>
-            <p>Skjeggtrimming</p>
-            <p>fra 500,-</p>
-          </div>
-          <div>
-            <p>Barneklipp</p>
-            <p>fra 400,-</p>
-          </div>
-          <div>
-            <p>Senior hårklipp</p>
-            <p>fra 400,-</p>
-          </div>
-          <a href="">Bestill Time</a>
-        <a href="">Les Mer</a>
         </div>
-        <div className="map-text">
-          <p id="price-info-text">Vi har et utvalgt ulike tjenester</p>
+        <div className="price-text">
+          <p id="price-info-text">Vi har et utvalg ulike tjenester</p>
           <h2>Prisliste</h2>
           <hr />
           <p>
@@ -116,7 +120,53 @@ function App() {
           </p>
         </div>
       </div>
-      {/* Add next: Contact Us, Map Location & Footer*/}
+      <div className="contact-us-container">
+        <div className="contact-text">
+          <h1>Kontakt Oss</h1>
+          <hr />
+          <p>Adresse hovedkontor</p>
+          <p>Åsenveien 3, 1400 Ski</p>
+        </div>
+        <div className="contact-form">
+          <div className="contact-form-input">
+            <input type="text" placeholder="Fornavn" />
+            <input type="text" placeholder="Etternavn" />
+            <input type="email" placeholder="E-post" />
+            <input type="tel" placeholder="Mobil" />
+          </div>
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            placeholder="Skriv til oss her..."
+          ></textarea>
+          <button>Send</button>
+        </div>
+      </div>
+      <div className="map-container">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1999.9097302588025!2d10.725182852038872!3d59.91704547071299!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46416dd779ba357b%3A0x677038c9acc2591c!2sDet%20kongelige%20slott!5e0!3m2!1sno!2sno!4v1677333174575!5m2!1sno!2sno"
+          title="map"
+          width="600"
+          height="450"
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
+      <footer>
+        <div className="footer-links">
+          <a href="">Bruksvilkår</a>
+          <a href="">Personerklæring</a>
+          <a href="">FAQ</a>
+          <a href="">Kontakt Oss</a>
+          <a href="">Last ned appen</a>
+        </div>
+        <div className="footer-copyright">
+          <p>© 2023 Michael Siddiqi</p>
+        </div>
+      </footer>
     </>
   );
 }
